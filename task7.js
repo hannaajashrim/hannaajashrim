97b7a1388f5819848659fd9cfd0695bf796db53e
hannajashrim <hannajashrim26@gmail.com>
2023-11-22 11:55:06 +0530

    first commit

---
+++ task7.js
@@ -0,0 +1,13 @@
+
+
+
+
+// let abc=document.querySelector(".heading1")
+// abc.innerHTML="Hello world";
+
+// abc.style.color="red"
+// abc.style.backgroundColor="grey";
+
+let styleObject={color:'red',"background-Color":'yellow'};
+Object.assign(abc.style, styleObject)
+
